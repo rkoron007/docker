9b5b59a89950239220097c72c18c0e1a2606e493
rkoron <rkoron007@gmail.com>
2019-10-11 15:02:13 -0700

    Add entrypoint script

--- demos/pushing_to_heroku/pokedex/entrypoint.sh
+++ demos/pushing_to_heroku/pokedex/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 # If Rails sees another server.pid it will think another Rails server is already 
 # runnning and will terminate.
-rm -f server.pid
+rm -f tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
